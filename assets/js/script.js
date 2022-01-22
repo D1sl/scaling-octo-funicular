5f61a61a123cd7614c87597f082950edd490b01a
D1sl <benjaminmolini@gmail.com>
2022-01-22 12:08:54 -0600

    Load block info from localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,24 @@ console.log("Hello");
 $(".saveBtn").on("click", function() {
     console.log("Save was clicked");
 
-    var time = $(this).siblings(".hour").text();
+    var time = $(this).parent().attr("id");
     var blockinfo = $(this).siblings(".blockinfo").val();
 
     console.log(time + " " + blockinfo);
 
+    localStorage.setItem(time, blockinfo);
+
 });
 
-var saveBlock = function() {
-    localStorage.setItem(time, blockinfo);
-}
+// Load blocks to the page
+$("#9 .blockinfo").val(localStorage.getItem("9"));
+$("#10 .blockinfo").val(localStorage.getItem("10"));
+$("#11 .blockinfo").val(localStorage.getItem("11"));
+$("#12 .blockinfo").val(localStorage.getItem("12"));
+$("#13 .blockinfo").val(localStorage.getItem("13"));
+$("#14 .blockinfo").val(localStorage.getItem("14"));
+$("#15 .blockinfo").val(localStorage.getItem("15"));
+$("#16 .blockinfo").val(localStorage.getItem("16"));
+$("#17 .blockinfo").val(localStorage.getItem("17"));
 
 // A loop that checks each time block once every second to see if it is in the past, present or future and changes its color accordingly
