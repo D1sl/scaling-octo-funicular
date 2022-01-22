a0333023e045c66cb3a9b89be74d3ae71c8c2f8b
D1sl <benjaminmolini@gmail.com>
2022-01-22 11:43:06 -0600

    Added time blocks, linked CSS and preliminary pseudocoding

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+console.log("Hello");
+// Pseudocoding
+
+// Time blocks are created and appended on the page
+
+// When a block is clicked, it turns into an input element
+
+// When user clicks out of that block or clicks the save icon, the event is saved
+
+// A loop that checks each time block once every second to see if it is in the past, present or future and changes its color accordingly
